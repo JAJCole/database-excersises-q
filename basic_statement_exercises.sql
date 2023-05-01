fb1b8b89730e701f6750bbe8286668a15189be38
Jarred Cole <jarred.aj.cole@gmail.com>
2023-05-01 12:27:49 -0500

    completed basic statement SQL prompts

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -13,11 +13,16 @@ select * from albums;
 /* The sales column represents the millions of copies of the albums sold. */
 
 # Find the name of all albums by Pink Floyd.
+select name, artist from albums where artist = 'Pink Floyd';
 
 # What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
+select release_date from albums where name = "Sgt. Pepper's Lonely Hearts Club Band";
 
 # What is the genre for the album Nevermind?
+select genre from albums where name = 'Nevermind';
 
 # Which albums were released in the 1990s?
+select name from albums where release_date between 1990 and 1999;
 
 # Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
+select name as low_selling_albums from albums where sales < 20;
