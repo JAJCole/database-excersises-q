b60d27dea54acf334bab6c869bbd5bbda841a0ef
Jarred Cole <jarred.aj.cole@gmail.com>
2023-05-01 12:17:01 -0500

    answer basic_statement SQL prompts

---
+++ basic_statement_exercises.sql
@@ -0,0 +1,23 @@
+# Use the albums_db database.
+use albums_db;
+
+# What is the primary key for the albums table?
+show tables;
+select * from albums;
+/* the primary key of the albums table is the "id" */
+
+# What does the column named 'name' represent?
+/* The column 'name' represents the name of the albums within the albums_db. */
+
+# What do you think the sales column represents?
+/* The sales column represents the millions of copies of the albums sold. */
+
+# Find the name of all albums by Pink Floyd.
+
+# What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
+
+# What is the genre for the album Nevermind?
+
+# Which albums were released in the 1990s?
+
+# Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
