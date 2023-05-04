3771cce23864ad4966ed73869dcfbcc1ab603be7
Jarred Cole <jarred.aj.cole@gmail.com>
2023-05-04 11:10:45 -0500

    group_by_exercises.sql

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,7 +10,7 @@ select distinct last_name from employees
 where last_name like 'E%E'
 group by last_name;
 
-/* Write a query to to find all unique combinations of first and last names of all employees 
+/* Write a query to find all unique combinations of first and last names of all employees 
 whose last names start and end with 'E'. */
 select distinct first_name, last_name from employees
 where last_name like 'E%E'
@@ -25,14 +25,19 @@ where last_name like '%q%' and last_name not like '%qu%';
 
 /* Add a COUNT() to your results (the query above) to find the number of employees 
 with the same last name. */
-select count(last_name) from employees
-where last_name like '%q%' and last_name not like '%qu%';
+select count(last_name),last_name from employees
+where last_name like '%q%' and last_name not like '%qu%'
+group by last_name
+order by count(last_name);
+
 
 /* Find all employees with first names 'Irena', 'Vidya', or 'Maya'. 
 Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.*/
-select count(*), first_name, last_name, gender from employees
+select first_name, gender, count(*) as name_gender from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
-group by first_name, last_name, gender;
+group by first_name, gender
+order by name_gender
+;
 
 /* Using your query that generates a username for all of the employees, 
 generate a count employees for each unique username. */
